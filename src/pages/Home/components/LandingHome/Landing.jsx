a6da628175bbcafc6a2a8f18ec28a8e27d3bdb85
Hassan Elemam <hassannnelemammm@gmail.com>
2026-04-26 16:12:17 +0300

    update packages

--- src/pages/Home/components/LandingHome/Landing.jsx
+++ src/pages/Home/components/LandingHome/Landing.jsx
@@ -6,7 +6,6 @@ export default function LandingHome() {
     <section className={styles.hero}>
       <div className="container-fluid px-4">
         <div className="row align-items-center gy-5">
-
           <div className="col-lg-6">
             <h1 className={styles.title}>
               Learning is <span className={styles.highlight}>What You</span>
@@ -17,19 +16,22 @@ export default function LandingHome() {
             </h1>
 
             <div className="d-flex align-items-center gap-3 mt-4 flex-wrap">
+              <NavLink to="/register" className={`btn ${styles.primaryBtn}`}>
+                Start Free Trial
+                <span className="ms-2">
+                  <img src="/icons/btn-arrow.svg" alt="" />
+                </span>
+              </NavLink>
 
-                <NavLink to="/register" className={`btn ${styles.primaryBtn}`}>
-                  Start Free Trial
-                  <span className="ms-2">
-                    <img src="/icons/btn-arrow.svg" alt="" />
-                  </span>
-                </NavLink>
-
-            <NavLink to="/courses">
-              <button className={`btn ${styles.playBtn}`} aria-label="Play">
-                <img src="/icons/Symbol.svg" alt="play" className={styles.playIcon} />
-              </button>
-            </NavLink>
+              <NavLink to="/courses">
+                <button className={`btn ${styles.playBtn}`} aria-label="Play">
+                  <img
+                    src="/icons/Symbol.svg"
+                    alt="play"
+                    className={styles.playIcon}
+                  />
+                </button>
+              </NavLink>
               <div className={styles.watchText}>
                 Watch Our <br /> New Courses
               </div>
@@ -55,7 +57,6 @@ export default function LandingHome() {
                     />
                   </div>
                 </div>
-
               </div>
 
               <div className={`${styles.card} ${styles.cardStat}`}>
@@ -69,9 +70,14 @@ export default function LandingHome() {
                   <span className={styles.dot}></span>
                   <span className={styles.dot}></span>
                 </div>
-                <div className={styles.socialText}>36K+ <span>Enrolled</span></div>
+                <div className={styles.socialText}>
+                  36K+ <span>Enrolled</span>
+                </div>
                 <div className={styles.avatars}>
-                  <span></span><span></span><span></span><span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
                 </div>
               </div>
             </div>
